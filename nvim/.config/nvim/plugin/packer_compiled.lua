79589a46115927a32834d24bc6ec804d5827106e
rafeDraper <rafedraper@gmail.com>
2022-07-30 22:52:20 +0200

    packer

--- nvim/.config/nvim/plugin/packer_compiled.lua
+++ nvim/.config/nvim/plugin/packer_compiled.lua
@@ -1,75 +1,70 @@
 -- Automatically generated packer.nvim plugin loader code
 
-if vim.api.nvim_call_function("has", { "nvim-0.5" }) ~= 1 then
-	vim.api.nvim_command('echohl WarningMsg | echom "Invalid Neovim version for packer.nvim! | echohl None"')
-	return
+if vim.api.nvim_call_function('has', {'nvim-0.5'}) ~= 1 then
+  vim.api.nvim_command('echohl WarningMsg | echom "Invalid Neovim version for packer.nvim! | echohl None"')
+  return
 end
 
-vim.api.nvim_command("packadd packer.nvim")
+vim.api.nvim_command('packadd packer.nvim')
 
 local no_errors, error_msg = pcall(function()
-	local time
-	local profile_info
-	local should_profile = false
-	if should_profile then
-		local hrtime = vim.loop.hrtime
-		profile_info = {}
-		time = function(chunk, start)
-			if start then
-				profile_info[chunk] = hrtime()
-			else
-				profile_info[chunk] = (hrtime() - profile_info[chunk]) / 1e6
-			end
-		end
-	else
-		time = function(chunk, start) end
-	end
 
-	local function save_profiles(threshold)
-		local sorted_times = {}
-		for chunk_name, time_taken in pairs(profile_info) do
-			sorted_times[#sorted_times + 1] = { chunk_name, time_taken }
-		end
-		table.sort(sorted_times, function(a, b)
-			return a[2] > b[2]
-		end)
-		local results = {}
-		for i, elem in ipairs(sorted_times) do
-			if not threshold or threshold and elem[2] > threshold then
-				results[i] = elem[1] .. " took " .. elem[2] .. "ms"
-			end
-		end
+  local time
+  local profile_info
+  local should_profile = false
+  if should_profile then
+    local hrtime = vim.loop.hrtime
+    profile_info = {}
+    time = function(chunk, start)
+      if start then
+        profile_info[chunk] = hrtime()
+      else
+        profile_info[chunk] = (hrtime() - profile_info[chunk]) / 1e6
+      end
+    end
+  else
+    time = function(chunk, start) end
+  end
+  
+local function save_profiles(threshold)
+  local sorted_times = {}
+  for chunk_name, time_taken in pairs(profile_info) do
+    sorted_times[#sorted_times + 1] = {chunk_name, time_taken}
+  end
+  table.sort(sorted_times, function(a, b) return a[2] > b[2] end)
+  local results = {}
+  for i, elem in ipairs(sorted_times) do
+    if not threshold or threshold and elem[2] > threshold then
+      results[i] = elem[1] .. ' took ' .. elem[2] .. 'ms'
+    end
+  end
 
-		_G._packer = _G._packer or {}
-		_G._packer.profile_output = results
-	end
+  _G._packer = _G._packer or {}
+  _G._packer.profile_output = results
+end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/home/rafa/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/home/rafa/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/home/rafa/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/home/rafa/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/home/rafa/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
+local package_path_str = "/home/garcra/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/home/garcra/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/home/garcra/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/home/garcra/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/home/garcra/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
 
-	if not string.find(package.cpath, install_cpath_pattern, 1, true) then
-		package.cpath = package.cpath .. ";" .. install_cpath_pattern
-	end
+if not string.find(package.cpath, install_cpath_pattern, 1, true) then
+  package.cpath = package.cpath .. ';' .. install_cpath_pattern
+end
 
-	time([[Luarocks path setup]], false)
-	time([[try_loadstring definition]], true)
-	local function try_loadstring(s, component, name)
-		local success, result = pcall(loadstring(s), name, _G.packer_plugins[name])
-		if not success then
-			vim.schedule(function()
-				vim.api.nvim_notify(
-					"packer.nvim: Error running " .. component .. " for " .. name .. ": " .. result,
-					vim.log.levels.ERROR,
-					{}
-				)
-			end)
-		end
-		return result
-	end
+time([[Luarocks path setup]], false)
+time([[try_loadstring definition]], true)
+local function try_loadstring(s, component, name)
+  local success, result = pcall(loadstring(s), name, _G.packer_plugins[name])
+  if not success then
+    vim.schedule(function()
+      vim.api.nvim_notify('packer.nvim: Error running ' .. component .. ' for ' .. name .. ': ' .. result, vim.log.levels.ERROR, {})
+    end)
+  end
+  return result
+end
 
 time([[try_loadstring definition]], false)
 time([[Defining packer_plugins]], true)
@@ -130,7 +125,7 @@ _G.packer_plugins = {
     url = "https://github.com/hrsh7th/cmp-path"
   },
   ["cmp-tabnine"] = {
-    config = { "\27LJ\2\np\0\0\5\0\4\0\b6\0\0\0'\2\1\0B\0\2\2\18\3\0\0009\1\2\0005\4\3\0B\1\3\1K\0\1\0\1\0\3\tsort\2\14max_lines\3è\a\20max_num_results\3\20\nsetup\23cmp_tabnine.config\frequire\0" },
+    config = { "\27LJ\2\np\0\0\5\0\4\0\b6\0\0\0'\2\1\0B\0\2\2\18\3\0\0009\1\2\0005\4\3\0B\1\3\1K\0\1\0\1\0\3\20max_num_results\3\20\14max_lines\3è\a\tsort\2\nsetup\23cmp_tabnine.config\frequire\0" },
     loaded = true,
     path = "/home/garcra/.local/share/nvim/site/pack/packer/start/cmp-tabnine",
     url = "https://github.com/tzachar/cmp-tabnine"
@@ -400,29 +395,13 @@ _G.packer_plugins = {
 time([[Defining packer_plugins]], false)
 -- Config for: cmp-tabnine
 time([[Config for cmp-tabnine]], true)
-try_loadstring("\27LJ\2\np\0\0\5\0\4\0\b6\0\0\0'\2\1\0B\0\2\2\18\3\0\0009\1\2\0005\4\3\0B\1\3\1K\0\1\0\1\0\3\tsort\2\14max_lines\3è\a\20max_num_results\3\20\nsetup\23cmp_tabnine.config\frequire\0", "config", "cmp-tabnine")
+try_loadstring("\27LJ\2\np\0\0\5\0\4\0\b6\0\0\0'\2\1\0B\0\2\2\18\3\0\0009\1\2\0005\4\3\0B\1\3\1K\0\1\0\1\0\3\20max_num_results\3\20\14max_lines\3è\a\tsort\2\nsetup\23cmp_tabnine.config\frequire\0", "config", "cmp-tabnine")
 time([[Config for cmp-tabnine]], false)
 if should_profile then save_profiles() end
 
-	time([[Defining packer_plugins]], false)
-	-- Config for: cmp-tabnine
-	time([[Config for cmp-tabnine]], true)
-	try_loadstring(
-		"\27LJ\2\np\0\0\5\0\4\0\b6\0\0\0'\2\1\0B\0\2\2\18\3\0\0009\1\2\0005\4\3\0B\1\3\1K\0\1\0\1\0\3\20max_num_results\3\20\14max_lines\3è\a\tsort\2\nsetup\23cmp_tabnine.config\frequire\0",
-		"config",
-		"cmp-tabnine"
-	)
-	time([[Config for cmp-tabnine]], false)
-	if should_profile then
-		save_profiles()
-	end
 end)
 
 if not no_errors then
-	error_msg = error_msg:gsub('"', '\\"')
-	vim.api.nvim_command(
-		'echohl ErrorMsg | echom "Error in packer_compiled: '
-			.. error_msg
-			.. '" | echom "Please check your config for correctness" | echohl None'
-	)
+  error_msg = error_msg:gsub('"', '\\"')
+  vim.api.nvim_command('echohl ErrorMsg | echom "Error in packer_compiled: '..error_msg..'" | echom "Please check your config for correctness" | echohl None')
 end
